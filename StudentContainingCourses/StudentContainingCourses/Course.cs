ddee6456dd07a062f58cdea1556b902d4dc2fbe6
Ana Mendes <anamendes23@gmail.com>
2018-04-05 10:22:56 -0700

    first commit

---
+++ StudentContainingCourses/StudentContainingCourses/Course.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentContainingCourses
+{
+    public class Course
+    {
+        //private fields
+        private string _courseName;
+        private string _courseNumber;
+        private string _instructor;
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private int _credits;
+        //static field to hold max number of students
+        internal static int _capacity;
+        internal static int _enrolled;
+        //constructor
+        public Course(string courseName, string courseNumber, string instructor,
+                        DateTime startDate, DateTime endDate, int credits)
+        {
+            _courseName = courseName;
+            _courseNumber = courseNumber;
+            _instructor = instructor;
+            _startDate = startDate;
+            _endDate = endDate;
+            _credits = credits;
+        }
+        //static constructor
+        static Course()
+        {
+            _capacity = 20;
+            _enrolled = 0;
+        }
+        //properties
+        public string CourseName
+        { get { return _courseName; } }
+        public string CourseNumber
+        { get { return _courseNumber; } }
+        public string Instructor
+        { get { return _instructor; } }
+        public DateTime StartDate
+        { get { return _startDate; } }
+        public DateTime EndDate
+        { get { return _endDate; } }
+        public int Credits
+        { get { return _credits; } }
+    }
+}
---
+++ StudentContainingCourses/StudentContainingCourses/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentContainingCourses
+{
+    class Student
+    {
+        //fields
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private List<Course> _courseList = new List<Course>();
+
+        //constructor
+        public Student(string firstname, string lastName, string id)
+        {
+            _firstName = firstname;
+            _lastName = lastName;
+            _id = id;
+        }
+        //properties
+        public string FirstName
+        { get { return _firstName; } }
+        public string LastName
+        { get { return _lastName; } }
+        public string ID
+        { get { return _id; } }
+        public Course[] CourseList
+        { get { return _courseList.ToArray(); } }
+        //methods
+        public void AddCourse(Course course)
+        {
+            //before adding this course, make sure there are enough seats
+            //to access static fields, you have to call the class instead of the object
+            if (Course._enrolled < Course._capacity)
+            {
+                //add course
+                _courseList.Add(course);
+                //increment enrolled
+                Course._enrolled++;
+            }
+        }
+        public void DropCourse(string courseNumber)
+        {
+            //get the course with the given courseNumber
+            Course course = GetCourse(courseNumber);
+            //remove from the _courseList, but check that it is not null
+            if (course != null)
+            {
+                //remove from list
+                _courseList.Remove(course);
+            }
+        }
+        //helper method
+        private Course GetCourse(string courseNumber)
+        {
+            foreach (Course course in _courseList)
+            {
+                if(course.CourseNumber == courseNumber)
+                {
+                    return course;
+                }
+            }
+            //not found
+            return null;
+        }
+    }
+}
