d3dc2fc60e85841c85a1017215984f98c7e9e5ec
anamendes23 <anamendes23@gmail.com>
2018-06-04 11:02:08 -0700

    Dictionary collection - new class

---
+++ Dictionary_Collection/Dictionary_Collection/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_Collection
+{
+    public class Account
+    {
+        private readonly int _accountNumber;
+        private decimal _balance;
+        private string _bank;
+
+        public Account(int accountNumber, decimal balance, string bank)
+        {
+            _accountNumber = accountNumber;
+            _balance = balance;
+            _bank = bank;
+        }
+        //public int AccountNumber { get { return _accountNumber; } }
+        public override string ToString()
+        {
+            //return String.Format("{0}\t\t{1}\t{2}",
+            //    _accountNumber, _balance.ToString("c"), _bank);
+
+            return $"{_accountNumber}\t{_balance:c}\t{_bank}";
+        }
+    }
+}
---
+++ Dictionary_Collection/Dictionary_Collection/Form1.cs
@@ -0,0 +1,34 @@
+﻿/* Ana Mendes
+ * anamendes23@gmail.com
+ * github.com/anamendes23
+ * */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary_Collection
+{
+    public partial class Form1 : Form
+    {
+        //create a Dictionary<TKey,TValue>
+        Dictionary<int, Account> accountDictionary = new Dictionary<int, Account>();
+
+        /* public void Add(TKey key, TValue value)
+         * 
+         * public bool TryGetValue(TKey key, out TValue value)
+         * 
+         * public bool Remove(TKey key)
+         * */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
+//read about hash functions and hashtables
