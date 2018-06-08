2c3145c7265c5aceff442c0251a30388b19808b1
anamendes23 <anamendes23@gmail.com>
2018-06-07 18:27:44 -0700

    added exemple to recursive method

--- Recursive_Method/Recursive_Method/Form1.Designer.cs
+++ Recursive_Method/Recursive_Method/Form1.Designer.cs
@@ -30,8 +30,10 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.BtnExample1 = new System.Windows.Forms.Button();
-            this.txtInput = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.BtnExample2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnExample3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -40,48 +42,68 @@
             this.listBox1.ItemHeight = 20;
             this.listBox1.Location = new System.Drawing.Point(39, 38);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(344, 364);
+            this.listBox1.Size = new System.Drawing.Size(222, 324);
             this.listBox1.TabIndex = 0;
             // 
             // BtnExample1
             // 
-            this.BtnExample1.Location = new System.Drawing.Point(419, 121);
+            this.BtnExample1.Location = new System.Drawing.Point(39, 377);
             this.BtnExample1.Name = "BtnExample1";
-            this.BtnExample1.Size = new System.Drawing.Size(183, 52);
+            this.BtnExample1.Size = new System.Drawing.Size(222, 52);
             this.BtnExample1.TabIndex = 1;
             this.BtnExample1.Text = "Example-1";
             this.BtnExample1.UseVisualStyleBackColor = true;
             this.BtnExample1.Click += new System.EventHandler(this.BtnExample1_Click);
             // 
-            // txtInput
+            // BtnExample2
             // 
-            this.txtInput.Location = new System.Drawing.Point(419, 78);
-            this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(183, 26);
-            this.txtInput.TabIndex = 3;
+            this.BtnExample2.Location = new System.Drawing.Point(297, 377);
+            this.BtnExample2.Name = "BtnExample2";
+            this.BtnExample2.Size = new System.Drawing.Size(192, 52);
+            this.BtnExample2.TabIndex = 5;
+            this.BtnExample2.Text = "Example-2";
+            this.BtnExample2.UseVisualStyleBackColor = true;
+            this.BtnExample2.Click += new System.EventHandler(this.BtnExample2_Click);
             // 
-            // label1
+            // richTextBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(419, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 20);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Enter max value";
+            this.richTextBox1.Location = new System.Drawing.Point(297, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(192, 324);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(521, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(316, 324);
+            this.panel1.TabIndex = 7;
+            // 
+            // BtnExample3
+            // 
+            this.BtnExample3.Location = new System.Drawing.Point(572, 377);
+            this.BtnExample3.Name = "BtnExample3";
+            this.BtnExample3.Size = new System.Drawing.Size(192, 52);
+            this.BtnExample3.TabIndex = 8;
+            this.BtnExample3.Text = "Example-3";
+            this.BtnExample3.UseVisualStyleBackColor = true;
+            this.BtnExample3.Click += new System.EventHandler(this.BtnExample3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(645, 441);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtInput);
+            this.ClientSize = new System.Drawing.Size(872, 441);
+            this.Controls.Add(this.BtnExample3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.BtnExample2);
             this.Controls.Add(this.BtnExample1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -89,8 +111,10 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button BtnExample1;
-        private System.Windows.Forms.TextBox txtInput;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnExample2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnExample3;
     }
 }
 
--- Recursive_Method/Recursive_Method/Form1.cs
+++ Recursive_Method/Recursive_Method/Form1.cs
@@ -63,6 +63,77 @@ namespace Recursive_Method
             //make a recursive call
             CountUp(counter + 1, topValue);
         }
+
+        private void BtnExample2_Click(object sender, EventArgs e)
+        {
+            //DisplayStarsHorizontally(10);
+            StarCountDown(10);
+        }
+        //define a recursive method that calls the DisplayStarsHorizontally
+        //so that multiple lines of stars are displayed where each next line
+        //has one less star, until you reach a single star in one line
+        private void StarCountDown(int n)
+        {
+            //define base condition to stop recursion
+            if (n < 1)
+                return;
+            //call display stars horizontally
+            DisplayStarsHorizontally(n);
+            //make a recursive call
+            StarCountDown(n - 1);
+        }
+
+        //given a method to display n stars in a single line
+        private void DisplayStarsHorizontally(int n)
+        {
+            //display n stars on a single line in a richtextbox
+            //richTextBox1.Clear();
+
+            for (int i = 1; i <= n; i++)
+                richTextBox1.AppendText("*");
+            richTextBox1.AppendText("\n");
+        }
+
+        //define Graphics object for the panel
+        Graphics g;
+
+        private void BtnExample3_Click(object sender, EventArgs e)
+        {
+            //crete it
+            g = panel1.CreateGraphics();
+            //set up the initial radius to be the max possible given the
+            //panel size
+            //set R = half shortest width and height
+            int R = Math.Min(panel1.Width, panel1.Height);
+            int xc = panel1.Width / 2;
+            int yc = panel1.Height / 2;
+
+            DrawCircle(R, xc, yc);
+            //call the recursive method
+        }
+        //define a method "drawCircle" that draws a circle with center set 
+        //in the middle of the panel
+        //(this method should have a single parameter radius)
+        public void DrawCircle(int r, int xc, int yc)
+        {
+            int x = xc - r;
+            int y = yc - r;
+            Pen pen = new Pen(Color.Red, 2);
+            g.DrawEllipse(pen, x, y, 2*r, 2*r);
+        }
+        //define a recursive method that calls the drawCircle to draw
+        //another circle with smaller radius. Continue to process until
+        //the smallest circle is about 5 pixels radius
+        private void DrawConcentrixCircles(int r, int xc, int yc)
+        {
+            //set up base
+            if (r < 5)
+                return;
+
+            //call drawcircle
+            DrawCircle(r, xc, yc);
+            //make recursive call for another inner circle
+        }
     }
 }
 ///Exercise
